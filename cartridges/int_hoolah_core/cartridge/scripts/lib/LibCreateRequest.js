0908896ea298caee8d60bdcd37d1f355914f7ab3
Thao Hoang <thao.hoang@isobar.com>
2020-08-21 22:54:57 +0700

    [HOOL-1] Make service call

---
+++ cartridges/int_hoolah_core/cartridge/scripts/lib/LibCreateRequest.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-undef */
+/** *******************************************************************************
+*
+* Description: Contains the functions to construct the request object for the
+* various Hoolah requests.
+*
+*
+/*********************************************************************************/
+var Logger = require('dw/system/Logger');
+var HoolahConstants = require('*/cartridge/scripts/common/HoolahConstants');
+var Resource = require('dw/web/Resource');
+
+function createAuthTokenService() {
+    var username = ''
+}
