aa7a1ff1f754e120823ae2ab935a0191c76886cb
Thao Hoang <thao.hoang@isobar.com>
2020-08-21 08:38:36 +0700

    [HOOL-1] Add pament method file template for Hoolah

---
+++ cartridges/int_hoolah_core/cartridge/scripts/common/HoolahConstants.js
@@ -0,0 +1,22 @@
+/**
+* All constants used in Hoolah
+*/
+function HoolahConstants() {}
+HoolahConstants.MALAY_AUTH_SERVICE='hoolah_auth_malay';
+HoolahConstants.MALAY_AUTH_SERVICE='hoolah_auth_sing';
+
+//Code Response
+HoolahConstants.NOTIFYCODE200='200';
+HoolahConstants.NOTIFYCODE201='201';
+HoolahConstants.NOTIFYCODE202='202';
+HoolahConstants.NOTIFYCODE400='400';
+HoolahConstants.NOTIFYCODE401='401';
+HoolahConstants.NOTIFYCODE404='404';
+HoolahConstants.NOTIFYCODE405='405';
+HoolahConstants.NOTIFYCODE406='406';
+HoolahConstants.NOTIFYCODE410='410';
+HoolahConstants.NOTIFYCODE412='412';
+HoolahConstants.NOTIFYCODE422='422';
+HoolahConstants.NOTIFYCODE429='429';
+HoolahConstants.NOTIFYCODE500='500';
+HoolahConstants.NOTIFYCODE503='503';
